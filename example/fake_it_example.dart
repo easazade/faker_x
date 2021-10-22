67686d03e3359ab1c87af38d643061f80784e8a9
alireza easazade <easazade@gmail.com>
2021-10-22 20:45:52 +0330

    initialization

---
+++ example/fake_it_example.dart
@@ -0,0 +1,6 @@
+import 'package:fake_it/fake_it.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/fake_it.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library fake_it;
+
+export 'src/fake_it_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.

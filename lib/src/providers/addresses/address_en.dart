0ee9b9008c2e20a4534954c4ab7da964445295ea
alireza easazade <easazade@gmail.com>
2023-01-17 12:51:23 +0330

    Add DataLists for addresses

---
+++ lib/src/providers/addresses/address_en.dart
@@ -0,0 +1,94 @@
+import 'package:fake_it/src/base/data_list.dart';
+import 'package:fake_it/src/base/format.dart';
+
+const citySuffixes = DataList(
+  values: [
+    '',
+  ],
+);
+
+const cityPrefixes = DataList(
+  values: [],
+);
+
+const cities = DataList(
+  formats: [],
+  values: [],
+);
+
+const streetSuffixes = DataList(
+  values: [],
+);
+
+const states = DataList(
+  values: [],
+);
+
+const postcodeFormats = DataList(
+  formats: [
+    Format('#####-####'),
+    Format('#####'),
+  ],
+  values: [],
+);
+
+const addresses = DataList(
+  formats: [
+    Format(''),
+  ],
+  values: [],
+);
+
+const neighborhoods = DataList(
+  values: [
+    'East of Telegraph Road',
+    'North Norridge',
+    'Northwest Midlothian/Midlothian Country Club',
+    'Mott Haven/Port Morris',
+    'Kingsbridge Heights',
+    'Bronxdale',
+    'Pennypack',
+    'Bridesburg',
+    'Allegheny West',
+    'Bushwick South',
+    'Dyker Heights',
+    'Ocean Parkway South',
+    'Summerlin North',
+    'Seven Hills Area',
+    'Greater Las Vegas National',
+    'phoenix',
+    'Central Chandler',
+    'South of Bell Road',
+    'River Heights',
+    'White Plains Central',
+    'Mount Kisco West',
+    'Pound Ridge East',
+    'Babylon Bayside',
+    'Sagaponack Seaside',
+    'South of Lake Ave',
+    'Far Rockaway/Bayswater',
+    'Jamaica Estates/Holliswood',
+    'Murray Hill',
+    'East Renton',
+    'Renton West',
+    'Auburn North',
+    'Northwoods West',
+    'Florissant West',
+    'Ladue South',
+    'Candlewood Country Club',
+    'West Covina East',
+    'North East Irwindale',
+    'Sunshine-Gardens',
+    'Cipriani',
+    'Brentwood Central',
+    'Jupiter South/Abacoa',
+    'Sea Ranch Lakes',
+    'Schall Circle/Lakeside Green',
+    'Olmsted Falls Central',
+    'South of Lake Shore Blvd',
+    'Gates Mills North',
+    'White Oak South of Columbia Pike',
+    'Rockville East of Hungerford Dr',
+    'Cleveland Park',
+  ],
+);
--- lib/src/providers/numbers/numbers.dart
+++
@@ -1,2 +0,0 @@
-// we don't need localized data for numbers just convert numbers to farsi other languages digits
-// in number faker if needed

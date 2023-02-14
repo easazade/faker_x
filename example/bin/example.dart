397826934e92902dbba1f472313532f5234bade7
alireza easazade <easazade@gmail.com>
2023-02-14 10:21:10 +0330

    Add example console app

---
+++ example/bin/example.dart
@@ -0,0 +1,53 @@
+import 'package:fake_it/fake_it.dart';
+
+void main(List<String> arguments) {
+  print('stateAbbr = ${FakeIt.localized.en_us.address.stateAbbr}');
+  print('state = ${FakeIt.localized.en_us.address.state}');
+  print('address = ${FakeIt.localized.en_us.address.address}');
+  print('cityName = ${FakeIt.localized.en_us.address.cityName}');
+  print('neighborhood = ${FakeIt.localized.en_us.address.neighborhood}');
+  print('buildingNumber = ${FakeIt.localized.en_us.address.buildingNumber}');
+  print('citySuffix = ${FakeIt.localized.en_us.address.citySuffix}');
+  print('streetSuffix = ${FakeIt.localized.en_us.address.streetSuffix}');
+  print('streetAddress = ${FakeIt.localized.en_us.address.streetAddress}');
+  print('postcode = ${FakeIt.localized.en_us.address.postcode}');
+  print('streetName = ${FakeIt.localized.en_us.address.streetName}');
+  print('postcodeFormat = ${FakeIt.localized.en_us.address.postcodeFormat}');
+  print('cityPrefix = ${FakeIt.localized.en_us.address.cityPrefix}');
+  print(
+      'secondaryAddress = ${FakeIt.localized.en_us.address.secondaryAddress}');
+  print('jobSuffix = ${FakeIt.localized.en_us.job.jobSuffix}');
+  print('jobTitle = ${FakeIt.localized.en_us.job.jobTitle}');
+  print('jobPrefix = ${FakeIt.localized.en_us.job.jobPrefix}');
+  print('lastNameEn = ${FakeIt.localized.en_us.person.lastNameEn}');
+  print('firstNameEn = ${FakeIt.localized.en_us.person.firstNameEn}');
+  print('firstNameMale = ${FakeIt.localized.en_us.person.firstNameMale}');
+  print('fullName = ${FakeIt.localized.en_us.person.fullName}');
+  print('firstNameFemale = ${FakeIt.localized.en_us.person.firstNameFemale}');
+  print('lastName = ${FakeIt.localized.en_us.person.lastName}');
+  print('firstName = ${FakeIt.localized.en_us.person.firstName}');
+  print('licensePlate = ${FakeIt.localized.en_us.automotive.licensePlate}');
+  print('colorHex = ${FakeIt.localized.en_us.color.colorHex}');
+  print('colorName = ${FakeIt.localized.en_us.color.colorName}');
+  print('sentence = ${FakeIt.localized.en_us.lorem.sentence}');
+  print('word = ${FakeIt.localized.en_us.lorem.word}');
+  print('animalName = ${FakeIt.localized.en_us.animal.animalName}');
+  print('car = ${FakeIt.localized.en_us.vehicle.car}');
+  print('httpUrl = ${FakeIt.localized.en_us.internet.httpUrl}');
+  print('email = ${FakeIt.localized.en_us.internet.email}');
+  print('httpsUrl = ${FakeIt.localized.en_us.internet.httpsUrl}');
+  print('safeEmail = ${FakeIt.localized.en_us.internet.safeEmail}');
+  print('ipv4 = ${FakeIt.localized.en_us.internet.ipv4}');
+  print('domainSuffixes = ${FakeIt.localized.en_us.internet.domainSuffixes}');
+  print('userName = ${FakeIt.localized.en_us.internet.userName}');
+  print('disposableEmail = ${FakeIt.localized.en_us.internet.disposableEmail}');
+  print('macAddress = ${FakeIt.localized.en_us.internet.macAddress}');
+  print('emoji = ${FakeIt.localized.en_us.internet.emoji}');
+  print('ipv6 = ${FakeIt.localized.en_us.internet.ipv6}');
+  print('mailProvider = ${FakeIt.localized.en_us.internet.mailProvider}');
+  print('domainName = ${FakeIt.localized.en_us.internet.domainName}');
+  print(
+      'disposableMailProvider = ${FakeIt.localized.en_us.internet.disposableMailProvider}');
+  print('avatarUri = ${FakeIt.localized.en_us.image.avatarUri}');
+  print('geoLocation = ${FakeIt.localized.en_us.location.geoLocation}');
+}

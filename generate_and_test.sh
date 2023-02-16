c124ad000247bd763320261bd07efc9857906b23
alireza easazade <easazade@gmail.com>
2023-02-16 12:51:27 +0330

    Colorize generate_and_test script

--- generate_and_test.sh
+++ generate_and_test.sh
@@ -6,11 +6,11 @@ dart scripts/create_tests.dart &&
 dart scripts/generate_example.dart &&
 dart scripts/readme.dart &&
 echo '----------------------------------------------------------------' &&
-echo -e 'Formatting generated code \n' &&
+echo -e '\x1B[33mFormatting generated code\033[0m \n' &&
 dart format . &&
 echo '----------------------------------------------------------------' &&
-echo -e 'Analyzing generated code \n' &&
+echo -e '\x1B[33mAnalyzing generated code\033[0m \n' &&
 dart analyze . &&
 echo '----------------------------------------------------------------' &&
-echo -e 'Testing generated code \n' &&
+echo -e '\x1B[33mTesting generated code\033[0m \n' &&
 dart test .

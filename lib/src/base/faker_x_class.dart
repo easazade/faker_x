934471e242a63ab4432709e703f24ea15ec354ef
alireza easazade <easazade@gmail.com>
2023-03-16 05:54:37 +0330

    Format files

--- lib/src/base/faker_x_class.dart
+++ lib/src/base/faker_x_class.dart
@@ -1,20 +1,22 @@
 /// !!!! DO NOT MODIFY BY HAND !!!!
 ///
-/// This file is generated using scripts in scripts directory. any changes you make 
+/// This file is generated using scripts in scripts directory. any changes you make
 /// to this file will be removed when code generation takes effect.
-/// 
+///
 /// NOTE: Some part of some of the generated files are generated using mustache
 /// templates in templates directory.
 
 import 'package:faker_x/src/locales/en_us/en_us.dart';
 import 'package:faker_x/src/locales/fa_ir/fa_ir.dart';
-class FakerX {
-FakerX._();
 
-static late final defaultInstance = EnUsCollection();
-static late final localized = _Localized();
+class FakerX {
+  FakerX._();
+
+  static late final defaultInstance = EnUsCollection();
+  static late final localized = _Localized();
 }
+
 class _Localized {
-late final en_us = EnUsCollection();
-late final fa_ir = FaIrCollection();
+  late final en_us = EnUsCollection();
+  late final fa_ir = FaIrCollection();
 }
